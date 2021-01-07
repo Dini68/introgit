cbe4ce8d5fd4816bf0df3c9d18b89035c8e402ef
Dini68 <kdini68@gmail.com>
2021-01-07 19:45:16 +0100

    gyakorlás

---
+++ src/main/java/training/aaa/pr.java
@@ -0,0 +1,11 @@
+package training.aaa;
+
+public interface pr {
+
+//    public void doMore(long bow);
+
+  //  public void doMore(long bow) {}
+//(  )  private short hop = 23;
+//(X)  public Name();
+
+}
